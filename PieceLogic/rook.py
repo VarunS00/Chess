738e9d9f6cc289921547a7a624b48075eacc66ec
Varun <vsrin00@gmail.com>
2026-06-19 11:26:27 -0400

    Rook

--- PieceLogic/rook.py
+++ PieceLogic/rook.py
@@ -1,46 +1,46 @@
 from PieceLogic.piece import Piece
 #Creates the Rook class, which inherits from Piece
 class Rook(Piece):
-
     def __init__(self, row, col, color, image=None):
+        #Calls the Piece constructor to initialize common attributes
         super().__init__(row, col, color, image)
-
+        #Sets the name attribute to "rook"
         self.name = "rook"
-
+    #Returns legal moves for the rook
     def get_moves(self, board):
-
+        #Creates an empty list to store moves
         moves = []
-
+        #Defines the possible directions the rook can move (up, down, left, right)
         directions = [
             (-1, 0),
             (1, 0),
             (0, -1),
             (0, 1)
         ]
-
+        #Iterates through each direction to calculate possible moves
         for dr, dc in directions:
-
+            #Initializes the row and column to the current position of the rook
             r = self.row
             c = self.col
-
+            #Moves in the current direction until it goes out of bounds or encounters a piece
             while True:
 
                 r += dr
                 c += dc
-
+                #Checks if the new position is within the boundaries of the board
                 if not board.in_bounds(r, c):
                     break
-
+                #Gets the piece at the new position
                 target = board.get_piece(r, c)
-
+                #If there is no piece at the new position, it is a valid move
                 if target is None:
                     moves.append((r, c))
 
                 else:
-
+                    #If there is a piece at the new position, check if it is an enemy piece
                     if target.color != self.color:
                         moves.append((r, c))
 
                     break
-
+        #Returns the list of legal moves for the rook
         return moves
